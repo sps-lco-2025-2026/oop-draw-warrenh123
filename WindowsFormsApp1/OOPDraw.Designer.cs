898ed644f203d208696016a11dd28df2b8bb1a45
warrenh123 <huw@stpaulsschool.org.uk>
2026-01-28 10:05:03 +0000

    oop

---
+++ WindowsFormsApp1/OOPDraw.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class OOPDraw
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Canvas = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.Canvas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Canvas
+            // 
+            this.Canvas.BackColor = System.Drawing.SystemColors.Window;
+            this.Canvas.Location = new System.Drawing.Point(244, 9);
+            this.Canvas.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Canvas.Name = "Canvas";
+            this.Canvas.Size = new System.Drawing.Size(609, 376);
+            this.Canvas.TabIndex = 0;
+            this.Canvas.TabStop = false;
+            this.Canvas.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            // 
+            // OOPDraw
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1111, 396);
+            this.Controls.Add(this.Canvas);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "OOPDraw";
+            this.Text = "OOPDraw";
+            ((System.ComponentModel.ISupportInitialize)(this.Canvas)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Canvas;
+    }
+}
+
---
+++ WindowsFormsApp1/OOPDraw.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class OOPDraw : Form
+    {
+        public OOPDraw()
+        {
+            InitializeComponent();
+            DoubleBuffered = true; //Stops image flickering
+        }
+        Pen currentPen = new Pen(Color.Black);
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics gr = e.Graphics;
+            Point a = new Point(20, 30);
+            Point b = new Point(400, 500);
+            gr.DrawLine(currentPen, a, b);
+        }
+    }
+}
